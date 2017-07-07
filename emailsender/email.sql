cda36f91d8f9c495ffe8d4a8ac198c8da26443bf
Tural <tural.esger@gmail.com>
2017-07-08 01:14:16 +0400

    alter email.body varchar to longtext

--- emailsender/email.sql
+++ emailsender/email.sql
@@ -1,3 +1,18 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : MySQL
+Source Server Version : 50714
+Source Host           : localhost:3306
+Source Database       : email
+
+Target Server Type    : MYSQL
+Target Server Version : 50714
+File Encoding         : 65001
+
+Date: 2017-07-08 01:09:22
+*/
+
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
@@ -8,7 +23,7 @@ CREATE TABLE `email` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `receiver` varchar(255) DEFAULT NULL,
   `subject` varchar(255) DEFAULT NULL,
-  `body` varchar(255) DEFAULT NULL,
+  `body` longtext,
   `date` datetime DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
